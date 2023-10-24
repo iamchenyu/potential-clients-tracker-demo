b5b1b0aa697b4e65c7e3b53382209a413fb014b7
Chenyu Wang <jadeyw7@gmail.com>
2023-10-23 20:43:33 -0400

    stop counting days after archive & refine sorting in frontend

--- frontend/src/clients/clientsTable/ClientsTableRow.js
+++ frontend/src/clients/clientsTable/ClientsTableRow.js
@@ -2,7 +2,6 @@ import * as React from "react";
 import TableCell from "@mui/material/TableCell";
 import TableRow from "@mui/material/TableRow";
 import ClientDetail from "../clientTabs/ClientDetail";
-import moment from "moment";
 import "./ClientsTableRow.css";
 
 export default function ClientsTableRow({ client }) {
@@ -24,9 +23,7 @@ export default function ClientsTableRow({ client }) {
         </TableCell>
         <TableCell align="center">{client.current_status_name}</TableCell>
         <TableCell align="center">{client.from_channel_name}</TableCell>
-        <TableCell align="center">
-          {moment(new Date()).diff(moment(client.created_at), "days")}
-        </TableCell>
+        <TableCell align="center">{client.days}</TableCell>
         <TableCell align="center">
           {client.is_enrolled === false ? "No" : "Yes"}
         </TableCell>
